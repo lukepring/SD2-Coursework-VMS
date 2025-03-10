7b0023b8ef2edde03eea87402fdd174c754697bc
Luke Pring <hello@lukepr.ing>
2025-03-10 02:01:38 +0000

    Import xcode project

---
+++ SD2 Coursework VMS/SD2 Coursework VMS/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  SD2 Coursework VMS
+//
+//  Created by Luke Pring & Jack Turner on 10/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
