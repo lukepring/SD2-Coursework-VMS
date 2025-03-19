a2b31263196edbbac67a3bd31a418e815a2ddb18
Luke Pring <hello@lukepr.ing>
2025-03-19 09:11:53 +0000

    SQLIte in C++ now fetches data correctly
    Also added sample data to the database file vms.db

--- SD2 Coursework VMS/SD2 Coursework VMS/include/vms_database.hpp
+++ SD2 Coursework VMS/SD2 Coursework VMS/include/vms_database.hpp
@@ -7,26 +7,55 @@
 
 #include <sqlite3.h>
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 using namespace std;
 
 class VMSDatabase {
 private:
     string db_path = "";
+    static int callback(void *data, int argc, char **argv, char **azColName){
+        int i;
+        fprintf(stderr, "%s: ", (const char*)data);
+        for(i = 0; i<argc; i++){
+            printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+        }
+        printf("\n");
+        return 0;
+    }
 public:
+    
     VMSDatabase() {
+        
         sqlite3 *db;
            char *zErrMsg = 0;
            int rc;
+        char *sql;
+        const char* data = "Callback function called";
 
-           rc = sqlite3_open("test.db", &db);
+           db_path = (fs::path(__FILE__).parent_path().parent_path() / "db" / "vms.db").string();
+        
+           rc = sqlite3_open(db_path.c_str(), &db);
 
            if( rc ) {
               fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
               
            } else {
               fprintf(stderr, "Opened database successfully\n");
+             sql = "SELECT * from \"Appointment Management\";";
+               
+               rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
+               
+               if( rc != SQLITE_OK ){
+                    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+                    sqlite3_free(zErrMsg);
+               } else {
+                   fprintf(stdout, "Operation done successfully\n");
+               }
+               
            }
-           sqlite3_close(db);
+           //sqlite3_close(db);
     }
 };
