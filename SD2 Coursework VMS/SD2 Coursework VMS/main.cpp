454ede39bc8f345501fe63251a746f9505866bc6
Jack Turner <jackoturner06@icloud.com>
2025-04-09 00:58:00 +0100

    Update main.cpp

--- SD2 Coursework VMS/SD2 Coursework VMS/main.cpp
+++ SD2 Coursework VMS/SD2 Coursework VMS/main.cpp
@@ -6,30 +6,14 @@
 //
 
 #include <iostream>
-//#include "include/appointments.hpp"
+#include "include/appointments.hpp"
 #include "include/owners.hpp"
 #include "include/pets.hpp"
 #include "include/vms_database.hpp"
 
 using namespace std;
 
-/*int main(int argc, const char * argv[]) {
-    std::cout << "VMS v1.0.0\n---------\n";
-    VMSDatabase VMSDatabase;
-
-    std::cout << VMSDatabase.getData("pets")[0][2] << std::endl;
-    
-    
-    
-    VMSDatabase.printTable("owners");
-    
-    VMSDatabase.deleteRecord("owners", 5);
-    
-    // VMSDatabase.addRecord("owners", {"Roger", "12 Lane Street, London", "07365287615", "roger@mail.org"});
-    
-    return 0;
-    
-}*/
+VMSDatabase db;
 
 void displayMainMenu() {
     cout << "=============================\n";
