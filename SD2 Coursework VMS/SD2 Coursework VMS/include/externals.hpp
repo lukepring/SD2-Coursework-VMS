4f512e6cd8ab31e4558c7e6b66308f5acfbdce66
Jack Turner <jackoturner06@icloud.com>
2025-04-09 01:02:19 +0100

    Create externals.hpp
    So there are no duplicate redefinitions of VMSDatabase.

---
+++ SD2 Coursework VMS/SD2 Coursework VMS/include/externals.hpp
@@ -0,0 +1,14 @@
+//
+//  externals.hpp
+//  SD2 Coursework VMS
+//
+//  Written by Jack Turner.
+//
+
+#ifndef EXTERNALS_HPP
+#define EXTERNALS_HPP
+
+#include "vms_database.hpp"
+extern VMSDatabase db;
+
+#endif
