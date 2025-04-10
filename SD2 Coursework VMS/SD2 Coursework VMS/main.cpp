58594969ae708663cbfed928d98e4644004f05cf
Luke Pring <hello@lukepr.ing>
2025-04-10 02:17:53 +0100

    User Roles Login Now Works!

--- SD2 Coursework VMS/SD2 Coursework VMS/main.cpp
+++ SD2 Coursework VMS/SD2 Coursework VMS/main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  SD2 Coursework VMS
 //
-//  Written by Luke Pring & Jack Turner.
+//  Written by Luke Pring, Jack Turner and Richard Akole.
 //
 
 #include <iostream>
@@ -15,22 +15,51 @@ using namespace std;
 
 VMSDatabase db;
 
+string name;
+int accessLevel;
+
 void displayMainMenu() {
     cout << "=============================\n";
     cout << " Veterinary Management System\n";
     cout << "=============================\n";
     
-    // User Roles / Login
+    // User Roles / Login (Luke & Richard)
     
-    string nameInput = "";
-    string passwordInput = "";
+    bool loggedIn = false;
     
-    cout << "Name: ";
-    cin >> nameInput;
+    while (!loggedIn) {
     
-    cout << "Password: ";
-    cin >> passwordInput;
+        string nameInput = "";
+        string passwordInput = "";
+        
+        cout << "Name: ";
+        cin >> nameInput;
+        
+        cout << "Password: ";
+        cin >> passwordInput;
+        
+        vector staffData = db.getData("staff");
     
+    
+        for(vector record : staffData) {
+            if(nameInput == record[3]) {
+                if(passwordInput == record[4]) {
+                    name = record[3];
+                    accessLevel = stoi(record[2]);
+                    cout << "=============================\nWelcome, " << name << "\nAccess Level: " << record[1] << "\n";
+                    loggedIn = true;
+                    break;
+                } else {
+                    cout << "Incorrect password. Please try again.";
+                }
+            }
+        }
+    }
+    
+    
+    // Menu (Jack)
+
+    cout << "=============================\n";
     cout << "1. Manage Pets\n";
     cout << "2. Manage Owners (Coming Soon)\n";
     cout << "3. Manage Appointments (Coming Soon)\n";
@@ -41,6 +70,7 @@ void displayMainMenu() {
 
 int main() {
     int choice;
+    
 
     do {
         displayMainMenu();
